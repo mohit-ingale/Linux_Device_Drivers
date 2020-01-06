9b1d50cc6ad443ec8d0f241d3a31f326ccf8265b
mohit-ingale <mohitingale5@gmail.com>
2020-01-06 11:45:14 -0800

    Inital Commit Code

---
+++ Code/BB_LED_Blink/KERN_SRC/kernel_timer.c
@@ -0,0 +1,139 @@
+/*
+===============================================================================
+Driver Name		:		delay
+Author			:		MOHIT
+License			:		GPL
+Description		:		LINUX DEVICE DRIVER PROJECT
+===============================================================================
+*/
+
+#include"kernel_timer.h"
+#include <linux/sched.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/cdev.h>
+#include "ioctl.h"
+#include <linux/interrupt.h>
+#include <linux/gpio.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("MOHIT");
+int del=1;
+int val=1;
+void my_tasklet(unsigned long);
+DECLARE_TASKLET(tasky,my_tasklet,5);
+unsigned int gpioLED = 49;
+dev_t dev;
+struct cdev *my_cdev;
+struct timer_list stime;
+
+void my_tasklet(unsigned long data)
+{
+	printk("tasklet jiffies=%ld\tdata=%ld\n",jiffies,data);
+	printk("i am in tasklet\n");
+}
+
+void delay_create(unsigned long del1)
+{
+	printk("jiffies=%ld\tdata=%ld\n",jiffies,del1);
+	tasklet_schedule(&tasky);
+	/*val=!val;
+	gpio_set_value(49,val);
+	stime.expires=jiffies+del1*HZ;
+		stime.data=del;
+		stime.function=delay_create;
+		add_timer(&stime);
+	//del_timer(&stime);*/
+	return;
+}
+
+
+int delay_open(struct inode *inode, struct file *filp)
+{
+
+	printk("device opened successfully\n");
+	return 0;
+}
+int delay_release(struct inode *inode, struct file *filp)
+{
+	printk("device closed successfully\n");
+	return 0;
+}
+ssize_t delay_read(struct file *filp, char __user *ubuff, size_t size, loff_t *offset)
+{
+	char kbuff[]="hello user";
+
+	copy_to_user(ubuff,kbuff,sizeof(kbuff));
+	printk("data coppied successfully\n");
+	//int min_cnt;
+	return 0;
+
+}
+ssize_t delay_write(struct file *filp, const char __user *ubuff, size_t size, loff_t *offset)
+{
+	return 0;
+}
+long delay_ioctl(struct file *filp, unsigned int cmd, unsigned long data)
+{
+	switch(cmd)
+	{
+	case BASIC_DELAY:
+			del=data;
+			break;
+		default:
+			return -ENOTTY;
+	}
+	return 0;
+}
+
+struct file_operations fops={
+		.open=delay_open,
+		.release=delay_release,
+		.read=delay_read,
+		.write=delay_write,
+		.unlocked_ioctl=delay_ioctl,
+};
+
+static int __init delay_init(void)
+{
+	int major,minor,ret;
+	alloc_chrdev_region(&dev,0,1,"delay");
+	major=MAJOR(dev);
+	minor=MINOR(dev);
+	printk("Major No.= %d \t Minor No.=%d\n",major,minor);
+	printk("Initlializing Delay Module\n");
+	my_cdev=cdev_alloc();
+	my_cdev->ops=&fops;
+	init_timer(&stime);
+	printk("jiffies=%ld\n",jiffies);
+			stime.expires=jiffies+del*HZ;
+			stime.data=del;
+			stime.function=delay_create;
+			add_timer(&stime);
+	printk("HZ=%d\n",HZ);
+	/*ret=gpio_is_valid(gpioLED);
+	gpio_request(49,"sysfs");
+	gpio_direction_output(49,val);
+	gpio_export(49,false);*/
+	ret=cdev_add(my_cdev,dev,1);
+	if(ret)
+	{
+		printk("error adding device \n");
+		return -EFAULT;
+	}
+	return 0;
+}
+
+static void __exit delay_exit(void)
+{	
+	del_timer(&stime);
+	gpio_unexport(49);
+	cdev_del(my_cdev);
+	unregister_chrdev_region(dev,1);
+	printk("exiting delay module\n");
+	PINFO("EXIT\n");
+
+}
+
+module_init(delay_init);
+module_exit(delay_exit);
+
---
+++ Code/BB_LED_Blink/KERN_SRC/kernel_timer.mod.c
@@ -0,0 +1,45 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xaef42088, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0x93c799c7, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0xc5cf0b10, __VMLINUX_SYMBOL_STR(gpiod_unexport) },
+	{ 0x88aebce8, __VMLINUX_SYMBOL_STR(gpio_to_desc) },
+	{ 0x6c09c2a4, __VMLINUX_SYMBOL_STR(del_timer) },
+	{ 0x526f8e2b, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0x1bb31047, __VMLINUX_SYMBOL_STR(add_timer) },
+	{ 0x9580deb, __VMLINUX_SYMBOL_STR(init_timer_key) },
+	{ 0x4d07f3c9, __VMLINUX_SYMBOL_STR(cdev_alloc) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0xfaef0ed, __VMLINUX_SYMBOL_STR(__tasklet_schedule) },
+	{ 0xdb7305a1, __VMLINUX_SYMBOL_STR(__stack_chk_fail) },
+	{ 0x4f8b5ddb, __VMLINUX_SYMBOL_STR(_copy_to_user) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x7d11c268, __VMLINUX_SYMBOL_STR(jiffies) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "F216A99440F9FF9D03FBF5E");
